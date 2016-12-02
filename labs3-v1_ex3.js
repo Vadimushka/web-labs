0d72bd16679e5b01a4e659443f91a3f331f309ee
maks96 <evstigneev1996@list.ru>
2016-12-02 14:33:57 +0300

    refact

--- labs3-v1_ex3.js
+++
@@ -1,9 +0,0 @@
-function calculation(){
-	var x = parseInt(document.getElementById('x').value);
-	var y = parseInt(document.getElementById('y').value);
-	
-	
-	var result = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
-	
-	alert(result);
-}
